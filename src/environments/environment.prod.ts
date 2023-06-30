823076651b8186520031e99db917e391f8242919
hernan527 <hernan527@gmail.com>
2023-06-30 12:30:08 -0300

    server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://comparo.onrender.com' 
-  apiBaseUrl: 'http://localhost:5200' 
+  apiBaseUrl: 'https://clinicas-listado.onrender.com' 
+  // apiBaseUrl: 'http://localhost:5200' 
 };
