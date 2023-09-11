393d044424bf5b6554678ef53b0e775b7a8bc657
hernan527 <hernan527@gmail.com>
2023-09-11 14:48:38 -0300

    url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,5 @@
-// export const SERVER_URL = 'https://clinicas-listado.onrender.com';
-export const SERVER_URL = 'http://localhost:5200';
+export const SERVER_URL = 'https://clinicas-listado.onrender.com';
+// export const SERVER_URL = 'http://localhost:5200';
+
+
 
