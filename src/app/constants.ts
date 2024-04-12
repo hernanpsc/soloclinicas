9e49d07f9b1d739b6be90f8af5631ef93ec89eb5
hernanpsc <136007081+hernanpsc@users.noreply.github.com>
2024-04-12 18:24:27 -0300

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://clinicas-listado.onrender.com';
+export const SERVER_URL = 'https://servidor-queplan.onrender.com.com';
 // export const SERVER_URL = 'http://localhost:5200';
 
 
