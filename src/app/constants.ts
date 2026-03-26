2954fbb4067e84d2706cf188479c5912dbdeb869
hernanpsc <136007081+hernanpsc@users.noreply.github.com>
2026-03-25 22:29:35 -0300

    uodate SERVER URLK

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://clinicas-listado.onrender.com';
+export const SERVER_URL = 'https://servidorplus.saludok.com.ar';
 // export const SERVER_URL = 'http://localhost:5200';
 
 
